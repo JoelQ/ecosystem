fab07de2d9d911b4e7b597744eee20bf36e0c1cf
Joël Quenneville <joelq@thoughtbot.com>
2020-05-09 01:02:58 -0400

    Introduce minimal modeling
    This first introduces the concept of a world with foxes and rabbits. It
    also provides a minimal visual representation as a string of emoji.
    
    Note that elm-format wants to convert some emoji to UTF-8 code points
    while leaving the literal emoji in for others. I'm not sure why this is
    the case but it won't allow me to consistently enter either literal
    emojis or code points, instead forcing me into this half-and-half
    situation.

--- src/Main.elm
+++ src/Main.elm
@@ -5,4 +5,41 @@ import Html exposing (Html)
 
 main : Html a
 main =
-    Html.text "hello world"
+    view initialModel
+
+
+type alias Model =
+    List Cell
+
+
+initialModel : Model
+initialModel =
+    [ Fox, Rabbit, Empty, Empty, Empty, Rabbit ]
+
+
+type Cell
+    = Fox
+    | Rabbit
+    | Empty
+
+
+
+-- VIEW
+
+
+view : Model -> Html a
+view model =
+    Html.text <| String.join "" <| List.map cellSymbol model
+
+
+cellSymbol : Cell -> String
+cellSymbol cell =
+    case cell of
+        Fox ->
+            "\u{1F98A}"
+
+        Rabbit ->
+            "🐰"
+
+        Empty ->
+            ""
